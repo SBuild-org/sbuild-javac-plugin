75a675805fcca035be0692bf251a5ab656604aae
Tobias Roeser <le.petit.fou@web.de>
2014-02-17 00:17:05 +0100

    Updated buildfile and readme.

--- org.sbuild.plugins.javac/SBuild.scala
+++ org.sbuild.plugins.javac/SBuild.scala
@@ -1,17 +1,62 @@
 import de.tototec.sbuild._
 
+import de.tototec.sbuild.ant._
+import de.tototec.sbuild.ant.tasks._
+
 @version("0.7.1")
-@classpath("mvn:org.sbuild:org.sbuild.plugins.sbuildplugin:0.2.1")
+@classpath(
+  "mvn:org.sbuild:org.sbuild.plugins.sbuildplugin:0.2.2",
+  "mvn:org.apache.ant:ant:1.8.4",
+  "mvn:org.sbuild:org.sbuild.plugins.mavendeploy:0.1.0"
+)
 class SBuild(implicit _project: Project) {
-  
+
+  val namespace = "org.sbuild.plugins.javac"
+  val version = "0.0.9000"
+  val url = "https://github.com/SBuild-org/sbuild-javac-plugin"
+  val sourcesJar = s"target/${namespace}-${version}-sources.jar"
+  val sourcesDir = "src/main/scala"
+
+  Target("phony:all") dependsOn "jar" ~ sourcesJar
+
   import org.sbuild.plugins.sbuildplugin._
-  Plugin[SBuildPlugin] configure {
-    _.copy(
-      sbuildVersion = "0.7.1",
-      pluginClass = "org.sbuild.plugins.javac.Javac",
-      pluginVersion = "0.0.9000",
-      deps = Seq()
+  Plugin[SBuildPlugin] configure (_.copy(
+    sbuildVersion = "0.7.1",
+    pluginClass = s"${namespace}.Javac",
+    pluginVersion = version
+  ))
+
+
+  import org.sbuild.plugins.mavendeploy._
+  Plugin[MavenDeploy] configure (_.copy(
+    groupId = "org.sbuild",
+    artifactId = namespace,
+    version = version,
+    artifactName = Some("SBuild Javac Plugin"),
+    description = Some("An SBuild Plugin that provides Javac compiler support."),
+    repository = Repository.SonatypeOss,
+    scm = Option(Scm(url = url, connection = url)),
+    developers = Seq(Developer(id = "TobiasRoeser", name = "Tobias Roeser", email = "le.petit.fou@web.de")),
+    gpg = true,
+    licenses = Seq(License.Apache20),
+    url = Some(url),
+    files = Map(
+      "jar" -> s"target/${namespace}-${version}.jar",
+      "sources" -> s"target/${namespace}-${version}-sources.jar",
+      "javadoc" -> "target/fake.jar"
     )
+  ))
+
+  Target(sourcesJar) dependsOn s"scan:${sourcesDir}" ~ "LICENSE.txt" exec { ctx: TargetContext =>
+    AntZip(destFile = ctx.targetFile.get, fileSets = Seq(
+      AntFileSet(dir = Path(sourcesDir)),
+      AntFileSet(file = Path("LICENSE.txt"))
+    ))
+  }
+
+  Target("target/fake.jar") dependsOn "LICENSE.txt" exec { ctx: TargetContext =>
+    import de.tototec.sbuild.ant._
+    tasks.AntJar(destFile = ctx.targetFile.get, fileSet = AntFileSet(file = "LICENSE.txt".files.head))
   }
 
 }
